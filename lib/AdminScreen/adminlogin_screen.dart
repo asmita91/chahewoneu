0f2b64bb0066971dfca97c4a51cd6e3426bfb66d
deeya2002 <diyadangol2002@gmail.com>
2022-12-09 10:49:03 +0545

    Created admin login page

---
+++ lib/AdminScreen/adminlogin_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AdminLoginScreen extends StatefulWidget {
+  const AdminLoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AdminLoginScreen> createState() => _AdminLoginScreenState();
+}
+
+class _AdminLoginScreenState extends State<AdminLoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
