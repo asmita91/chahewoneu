bbe2cf6ace37ece69c0519540ddc80bff7bec922
Aayushbahadurpradhan <pradhanaayush9841@gmail.com>
2022-12-19 13:52:09 +0545

    created admin hotel add dart file

---
+++ lib/AdminScreens/hoteladd.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class HotelAddScreen extends StatefulWidget {
+  const HotelAddScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HotelAddScreen> createState() => _HotelAddScreenState();
+}
+
+class _HotelAddScreenState extends State<HotelAddScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
