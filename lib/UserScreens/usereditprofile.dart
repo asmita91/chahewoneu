ba6ad84c4cc8717418b147e932773949d5faea76
deeya2002 <diyadangol2002@gmail.com>
2022-12-20 17:11:41 +0545

    edit profile changes

--- lib/UserScreens/usereditprofile.dart
+++ lib/UserScreens/usereditprofile.dart
@@ -1,14 +1,139 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'dart:io';
 
-class EditProfile extends StatefulWidget {
-  const EditProfile({super.key});
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+
+class EditProfileScreen extends StatefulWidget {
+  const EditProfileScreen({super.key});
 
   @override
-  State<EditProfile> createState() => _EditProfileState();
+  State<EditProfileScreen> createState() => _EditProfileScreenState();
 }
 
-class _EditProfileState extends State<EditProfile> {
+class _EditProfileScreenState extends State<EditProfileScreen> {
+  TextEditingController name = new TextEditingController();
+  TextEditingController email = new TextEditingController();
+  TextEditingController password = new TextEditingController();
+  TextEditingController contact = new TextEditingController();
+  File? pickedImage;
+  bool changePaswordState = false;
+  final form = GlobalKey<FormState>();
+  showHidePassword() {
+    setState(() {
+      changePaswordState = !changePaswordState;
+    });
+  }
+
+  Widget showVisibilityIcon(bool showPassword) {
+    return showPassword == changePaswordState
+        ? InkWell(
+            onTap: () {
+              setState(() {
+                changePaswordState = !changePaswordState;
+              });
+            },
+            child: Icon(
+              Icons.remove_red_eye,
+              color: Colors.white,
+            ))
+        : InkWell(
+            onTap: () {
+              setState(() {
+                changePaswordState = !changePaswordState;
+              });
+            },
+            child: Icon(
+              Icons.panorama_fish_eye,
+              color: Colors.white,
+            ));
+  }
+
+  hintStyle() {
+    TextStyle(
+      fontWeight: FontWeight.bold,
+      color: Colors.white,
+    );
+  }
+
+  formsOutline() {
+    OutlineInputBorder(
+        borderSide: BorderSide(width: 2, color: Colors.white),
+        borderRadius: BorderRadius.circular(30));
+  }
+
+  void imagePickerOption() {
+    Get.bottomSheet(
+      SingleChildScrollView(
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(10.0),
+            topRight: Radius.circular(10.0),
+          ),
+          child: Container(
+            color: Colors.white,
+            height: 250,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const Text(
+                    "Pic Image From",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      pickImage(ImageSource.camera);
+                    },
+                    icon: const Icon(Icons.camera),
+                    label: const Text("CAMERA"),
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      pickImage(ImageSource.gallery);
+                    },
+                    icon: const Icon(Icons.image),
+                    label: const Text("GALLERY"),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      Get.back();
+                    },
+                    icon: const Icon(Icons.close),
+                    label: const Text("CANCEL"),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  pickImage(ImageSource imageType) async {
+    try {
+      final photo = await ImagePicker().pickImage(source: imageType);
+      if (photo == null) return;
+      final tempImage = File(photo.path);
+      setState(() {
+        pickedImage = tempImage;
+      });
+
+      Get.back();
+    } catch (error) {
+      debugPrint(error.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
