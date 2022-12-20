2d7958ba9dd43736c24a9ea1409d4a9e0686e0b9
deeya2002 <diyadangol2002@gmail.com>
2022-12-20 16:52:52 +0545

    adding edit profile

---
+++ lib/UserScreens/usereditprofile.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class EditProfile extends StatefulWidget {
+  const EditProfile({super.key});
+
+  @override
+  State<EditProfile> createState() => _EditProfileState();
+}
+
+class _EditProfileState extends State<EditProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
