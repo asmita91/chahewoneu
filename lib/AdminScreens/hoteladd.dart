487b226cdd3eeff3e2707850fe86025cc2d416cd
Aayushbahadurpradhan <pradhanaayush9841@gmail.com>
2022-12-20 15:02:15 +0545

    addded image picker in hotel add

--- lib/AdminScreens/hoteladd.dart
+++ lib/AdminScreens/hoteladd.dart
@@ -1,15 +1,147 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:html';
 
-class HotelAddScreen extends StatefulWidget {
-  const HotelAddScreen({Key? key}) : super(key: key);
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HotelAddPlace extends StatefulWidget {
+  const  HotelAddPlace ({super.key});
 
   @override
-  State<HotelAddScreen> createState() => _HotelAddScreenState();
+  State< HotelAddPlace > createState() => _HotelPlace ();
 }
 
-class _HotelAddScreenState extends State<HotelAddScreen> {
+class  _HotelPlace  extends State< HotelAddPlace > {
+  TextEditingController place = new TextEditingController();
+  TextEditingController place_description = new TextEditingController();
+  File? pickedImage;
+
+  void imagePickerOption() {
+    Get.bottomSheet(
+      SingleChildScrollView(
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(0.0),
+            topRight: Radius.circular(0.0),
+          ),
+          child: Container(
+            color: Colors.blue,
+            height: 250,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const Text(
+                    "Pic Image From",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      pickImage(ImageSource.camera);
+                    },
+                    icon: const Icon(Icons.camera),
+                    label: const Text("CAMERA"),
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      pickImage(ImageSource.gallery);
+                    },
+                    icon: const Icon(Icons.image),
+                    label: const Text("GALLERY"),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      Get.back();
+                    },
+                    icon: const Icon(Icons.close),
+                    label: const Text("CANCEL"),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  pickImage(ImageSource imageType) async {
+    try {
+      final photo = await ImagePicker().pickImage(source: imageType);
+      if (photo == null) return;
+      final tempImage = File(photo.path);
+      setState(() {
+        pickedImage = tempImage;
+      });
+
+      Get.back();
+    } catch (error) {
+      debugPrint(error.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Add Hotel'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const SizedBox(
+            height: 10,
+          ),
+
+          Align(
+            alignment: Alignment.center,
+            child: Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        color: Colors.grey.withOpacity(0.6), width: 2),
+                  ),
+                  child: ClipRect(
+                    child: pickedImage != null
+                        ? Image.file(
+                      pickedImage!,
+                      width: 500,
+                      height: 200,
+                      fit: BoxFit.cover,
+                    )
+                        : Image.network(
+                      'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/-Insert_image_here-.svg/320px--Insert_image_here-.svg.png?20220802103107',
+                      // width: 500,
+                      // height: 800,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton.icon(
+                onPressed: imagePickerOption,
+                icon: const Icon(Icons.add_a_photo_sharp),
+                label: const Text('Hotel Image')),
+          ),
+        ],
+      ),
+    );
   }
 }
+
