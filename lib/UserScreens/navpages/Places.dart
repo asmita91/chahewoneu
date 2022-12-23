f2ae00a2e07b4017fdf2aad4ffb17dbdef9ea01a
unknown <katelasmita91@gmail.com>
2022-12-23 10:51:35 +0545

    places file made functional

--- lib/UserScreens/navpages/Places.dart
+++ lib/UserScreens/navpages/Places.dart
@@ -14,7 +14,8 @@ class _PlaceDetailsState extends State<PlaceDetails>
     with TickerProviderStateMixin {
   PageController pageController = PageController(viewportFraction: 0.85);
   var _currentPageValue = 0.0;
-  double scaleFactor = 0.8; // show the 80% of original size
+  double _scaleFactor = 0.8; // show the 80% of original size
+  double _height = 220;
   @override
   // for zooming of each container
   // method inside staful cls only
@@ -51,42 +52,61 @@ class _PlaceDetailsState extends State<PlaceDetails>
     //dart coding
     Matrix4 matrix = new Matrix4.identity();
     if (index == _currentPageValue.floor()) {
-      var currentScale = 1 - (_currentPageValue - index) * (1 - scaleFactor);
-      //1:44 bata resume
-      matrix = Matrix4.diagonal3Values(1, currentScale, 1);
+      var _currentScale = 1 - (_currentPageValue - index) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - _currentScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, _currentScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
     } else if (index == _currentPageValue.floor() + 1) {
-      var currentScale =
-          scaleFactor + (_currentPageValue - index + 1) * (1 - scaleFactor);
-      matrix = Matrix4.diagonal3Values(1, currentScale, 1);
+      var _currentScale =
+          _scaleFactor + (_currentPageValue - index + 1) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - _currentScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, _currentScale, 1);
+      matrix = Matrix4.diagonal3Values(1, _currentScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else if (index == _currentPageValue.floor() - 1) {
+      var _currentScale = 1 - (_currentPageValue - index) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - _currentScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, _currentScale, 1);
+      matrix = Matrix4.diagonal3Values(1, _currentScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else {
+      var _currentScale = 0.8;
+      matrix = Matrix4.diagonal3Values(1, _currentScale, 1)
+        ..setTranslationRaw(0, _height * (1 - _scaleFactor) / 2, 1);
     }
+
     return Transform(
       transform: matrix,
-      child: Stack(children: [
-        Container(
-          height: 200,
-          margin: EdgeInsets.only(left: 10, right: 10),
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: index.isEven ? Color(0xFF69C5DF) : Color(0xFF9294CC),
-              image: DecorationImage(
-                  fit: BoxFit.cover, image: AssetImage("Images/Pokhara.jpg"))),
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            height: 110,
-            margin: EdgeInsets.only(left: 50, right: 50, bottom: 30),
+      child: Container(
+        height: MediaQuery.of(context).size.height / 3,
+        child: Stack(children: [
+          Container(
+            height: 200,
+            margin: EdgeInsets.only(left: 10, right: 10, top: 10),
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: Colors.white,
-            ),
+                borderRadius: BorderRadius.circular(30),
+                color: index.isEven ? Color(0xFF69C5DF) : Color(0xFF9294CC),
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage("Images/Pokhara.jpg"))),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
             child: Container(
-              padding: EdgeInsets.only(top: 10, left: 15, right: 15),
-              child: AppColumn(),
+              height: 110,
+              margin: EdgeInsets.only(left: 50, right: 50, bottom: 30),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.white,
+              ),
+              child: Container(
+                padding: EdgeInsets.only(top: 10, left: 15, right: 15),
+                child: AppColumn(),
+              ),
             ),
           ),
-        ),
-      ]),
+        ]),
+      ),
     );
   }
 }
