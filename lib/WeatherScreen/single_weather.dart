ce897cdb2dc14f3495686b65d40972a3f1b2b71a
deeya2002 <diyadangol2002@gmail.com>
2022-12-27 17:14:03 +0545

    single weather and weather location codes written

--- lib/WeatherScreen/single_weather.dart
+++ lib/WeatherScreen/single_weather.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+class SingleWeather extends StatelessWidget {
+  final int index;
+  SingleWeather(this.index);
 
-class UserSingleWeather extends StatefulWidget {
-  const UserSingleWeather({Key? key}) : super(key: key);
-
-  @override
-  State<UserSingleWeather> createState() => _UserSingleWeatherState();
-}
-
-class _UserSingleWeatherState extends State<UserSingleWeather> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.all(20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Column(
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 40),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.white30,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
