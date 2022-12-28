ff951171eba47875224dc3a1f6b83e8cdd818f95
Ritikastha <ritikashres2244@gamil.com>
2022-12-28 14:14:01 +0545

    getImageFilePathFromAssets

--- lib/Navpages/NotificationPage.dart
+++ lib/Navpages/NotificationPage.dart
@@ -34,3 +34,12 @@ Navigator.of(context!).pushNamed(response.payload.toString());
 print(response.payload);
 }
 }
+static Future<String> getImageFilePathFromAssets(
+String asset, String filename) async {
+final byteData = await rootBundle.load(asset);
+final temp_direactory = await getTemporaryDirectory();
+final file = File('${temp_direactory.path}/$filename');
+await file.writeAsBytes(byteData.buffer.asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+
+return file.path;
+}
