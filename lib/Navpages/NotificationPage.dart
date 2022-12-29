e4fd9b0488536f8a6e06fc6ac8b12cc2a1ea376d
Ritikastha <ritikashres2244@gamil.com>
2022-12-29 13:34:20 +0545

    Download and save code

--- lib/Navpages/NotificationPage.dart
+++ lib/Navpages/NotificationPage.dart
@@ -81,3 +81,12 @@ NotificationDetails(
 );
 _notificationsPlugin.show(id, title, body, notificationDetails,payload: payload);
 }
+
+static Future<String> _downloadAndSaveFile(String url, String fileName) async {
+final Directory directory = await getApplicationDocumentsDirectory();
+final String filePath = '${directory.path}/$fileName';
+final http.Response response = await http.get(Uri.parse(url));
+final File file = File(filePath);
+await file.writeAsBytes(response.bodyBytes);
+return filePath;
+}
