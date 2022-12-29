c510fb3e86f393183d24c6818e2e111f1719b884
Ritikastha <ritikashres2244@gamil.com>
2022-12-29 13:37:42 +0545

    Download and save file code

--- lib/Navpages/NotificationPage.dart
+++ lib/Navpages/NotificationPage.dart
@@ -1,4 +1,3 @@
-
 import 'dart:io';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
@@ -11,7 +10,7 @@ class NotificationService{
   static final FlutterLocalNotificationsPlugin
   _notificationsPlugin = FlutterLocalNotificationsPlugin();
 
-  static void initialize() {
+  static void initialize(){
     final InitializationSettings initializationSettings =
     InitializationSettings(
         android: AndroidInitializationSettings("@mipmap/ic_launcher"),
@@ -21,72 +20,104 @@ class NotificationService{
             requestSoundPermission: false
         )
     );
+
+    _notificationsPlugin.initialize(initializationSettings,
+        onDidReceiveNotificationResponse: onDidReceiveNotificationResponse
+    );
   }
-  _notificationsPlugin.initialize(initializationSettings,
-  onDidReceiveNotificationResponse: onDidReceiveNotificationResponse
-  );
+  // static void onDidReceiveNotificationResponse
+  //     (NotificationResponse notificationResponse) async{
+  //   final String? payload = notificationResponse.payload;
+  //   print(payload);
+  // }
+
+  static BuildContext? context;
+  static void onDidReceiveNotificationResponse
+      (NotificationResponse? response) {
+    if (response != null && response.payload != null) {
+      Navigator.of(context!).pushNamed(response.payload.toString());
+      print(response.payload);
     }
-static BuildContext? context;
-static void onDidReceiveNotificationResponse
-(NotificationResponse? response) {
-if (response != null && response.payload != null) {
-Navigator.of(context!).pushNamed(response.payload.toString());
-print(response.payload);
-}
-}
-static Future<String> getImageFilePathFromAssets(
-String asset, String filename) async {
-final byteData = await rootBundle.load(asset);
-final temp_direactory = await getTemporaryDirectory();
-final file = File('${temp_direactory.path}/$filename');
-await file.writeAsBytes(byteData.buffer.asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+  }
 
-return file.path;
-}
-static void display(String title,String body,String payload,BuildContext _context) async{
-context=_context;
-// if image from asset
-//big picture title ko tala
-final bigpicture = await getImageFilePathFromAssets(
-'assets/images/logoicon.png', 'bigpicture');
+  static Future<String> getImageFilePathFromAssets(
+      String asset, String filename) async {
+    final byteData = await rootBundle.load(asset);
+    final temp_direactory = await getTemporaryDirectory();
+    final file = File('${temp_direactory.path}/$filename');
+    await file.writeAsBytes(byteData.buffer.asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
 
-//title ko side ma aaune simple image
-final smallpicture = await getImageFilePathFromAssets(
-'assets/images/logoicon.png', 'smallpicture');
+    return file.path;
+  }
 
-//scroll garesi aaune image
-final styleinformationDesign = BigPictureStyleInformation(
-  FilePathAndroidBitmap(smallpicture),
-  largeIcon: FilePathAndroidBitmap(bigpicture),
-);
-final id=DateTime.now().millisecondsSinceEpoch ~/1000;
-final NotificationDetails notificationDetails=
+  static void display(String title,String body,String payload,BuildContext _context) async{
+    context=_context;
+    // if image from asset
+    //big picture title ko tala
+    final bigpicture = await getImageFilePathFromAssets(
+        'assets/images/logoicon.png', 'bigpicture');
 
-NotificationDetails(
+    //title ko side ma aaune simple image
+    final smallpicture = await getImageFilePathFromAssets(
+        'assets/images/logoicon.png', 'smallpicture');
 
-    android :AndroidNotificationDetails(
-      //yo app ko anusra change garna parcha
-      //myapp channel andriod ma huncha so dinaiparne huncha
-        "myapp",
-        "myapp channel",
-        channelDescription:"Channel Description",
-        //importance and priority chai high and max rakhe vane notification background ma aaune ki pop up hune
-        importance : Importance.max,
-        priority:Priority.high,
-        styleInformation: styleinformationDesign
-    ),
+    //scroll garesi aaune image
+    final styleinformationDesign = BigPictureStyleInformation(
+      FilePathAndroidBitmap(smallpicture),
+      largeIcon: FilePathAndroidBitmap(bigpicture),
+    );
+
+    final id=DateTime.now().millisecondsSinceEpoch ~/1000;
+    final NotificationDetails notificationDetails=
+
+    NotificationDetails(
+
+        android :AndroidNotificationDetails(
+          //yo app ko anusra change garna parcha
+          //myapp channael andrio ma huncha so dinaiparne huncha
+            "myapp",
+            "myapp channel",
+            channelDescription:"Channel Description",
+            //importance and priority chai high and max rakhe vane notification background ma aaune ki pop up hune
+            importance : Importance.max,
+            priority:Priority.high,
+            styleInformation: styleinformationDesign
+        ),
 
 
-    iOS:DarwinNotificationDetails()
-);
-_notificationsPlugin.show(id, title, body, notificationDetails,payload: payload);
-}
+        iOS:DarwinNotificationDetails()
+    );
+    //Payload ma chai extra kei thapna cha vane use garna milcha
+    _notificationsPlugin.show(id, title, body, notificationDetails,payload: payload);
+  }
+  static Future<String> _downloadAndSaveFile(String url, String fileName) async {
+    final Directory directory = await getApplicationDocumentsDirectory();
+    final String filePath = '${directory.path}/$fileName';
+    final http.Response response = await http.get(Uri.parse(url));
+    final File file = File(filePath);
+    await file.writeAsBytes(response.bodyBytes);
+    return filePath;
+  }
 
-static Future<String> _downloadAndSaveFile(String url, String fileName) async {
-final Directory directory = await getApplicationDocumentsDirectory();
-final String filePath = '${directory.path}/$fileName';
-final http.Response response = await http.get(Uri.parse(url));
-final File file = File(filePath);
-await file.writeAsBytes(response.bodyBytes);
-return filePath;
+
+  static void displayFcm({required RemoteNotification notification, BuildContext? buildContext, String? payload}) async {
+    try {
+      if (buildContext != null) {
+        context = buildContext;
+      }
+      var styleinformationDesign;
+      if (notification.android!.imageUrl != null) {
+        final bigpicture = await _downloadAndSaveFile(
+            notification.android!.imageUrl.toString(), 'bigPicture');
+        final smallpicture = await _downloadAndSaveFile(
+            notification.android!.imageUrl.toString(), 'smallIcon');
+        styleinformationDesign = BigPictureStyleInformation(
+          FilePathAndroidBitmap(smallpicture),
+          largeIcon: FilePathAndroidBitmap(bigpicture),
+        );
+      }
+    }catch(e){
+      print(e.toString());
+    }
+  }
 }
