6ed307a7c3801605c837456383c51012a673dbc3
Sanjeelathapa7 <sanjeelathapamagar1234@gmail.com>
2022-12-29 10:29:15 +0545

    Hiace seat edited

--- lib/UserScreens/Transportation/ChooseHiaceSeat.dart
+++ lib/UserScreens/Transportation/ChooseHiaceSeat.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class ChooseHiaceSeat extends StatefulWidget {
   const ChooseHiaceSeat({Key? key}) : super(key: key);
@@ -17,7 +18,6 @@ class _ChooseHiaceSeatState extends State<ChooseHiaceSeat> {
 
   @override
   void initState() {
-
     initSeatValueToList(listSeatLeft, countSeatLeft, "l");
     initSeatValueToList(listSeatCenter, countSeatCenter, "c");
     initSeatValueToList(listSeatRight, countSeatRight, "r");
@@ -80,6 +80,183 @@ class _ChooseHiaceSeatState extends State<ChooseHiaceSeat> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        systemOverlayStyle: SystemUiOverlayStyle.light,
+      ),
+      body: Container(
+        width: MediaQuery
+            .of(context)
+            .size
+            .width,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              child: Text("Hiace Seat Booking"),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      child: Row(
+                        children: [
+                          Container(
+                            width: 20,
+                            height: 20,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              color: Color.fromARGB(255, 196, 44, 34),
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(left: 10),
+                            child: Text(
+                              "Booked",
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      child: Row(
+                        children: [
+                          Container(
+                            width: 20,
+                            height: 20,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              color: Colors.purple,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(left: 10),
+                            child: Text("Selected"),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      child: Row(
+                        children: [
+                          Container(
+                            width: 20,
+                            height: 20,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              border: Border.all(
+                                width: 1,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(left: 10),
+                            child: Text("available"),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 10),
+              // height: 100,
+              width: MediaQuery
+                  .of(context)
+                  .size
+                  .width,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  widgetSeat(listSeatLeft, false),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Expanded(
+                    child: widgetSeat(listSeatCenter, true),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  widgetSeat(listSeatRight, false),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setSelectedToBooked();
+              },
+              child: Text("Book"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget widgetSeat(List dataSeat, bool isCenter) {
+    return Container(
+      width: MediaQuery
+          .of(context)
+          .size
+          .width / 3.93,
+      child: GridView.builder(
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: isCenter ? 4 : 3,
+          childAspectRatio: isCenter ? 1 : 1,
+        ),
+        itemCount: dataSeat.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Visibility(
+            visible: dataSeat[index]["isVisible"],
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  dataSeat[index]["isSelected"] =
+                  !dataSeat[index]["isSelected"];
+                });
+              },
+              child: Container(
+                margin: EdgeInsets.all(5),
+                width: 20,
+                height: 20,
+                decoration: BoxDecoration(
+                  color: dataSeat[index]["isBooked"]
+                      ? Colors.red
+                      : dataSeat[index]["isSelected"]
+                      ? Colors.purple
+                      : Colors.transparent,
+                  border: Border.all(
+                    color: Colors.grey,
+                  ),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
