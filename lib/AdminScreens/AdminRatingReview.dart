9379806dbb30b13104f458651304fde4c70527c6
Aayushbahadurpradhan <pradhanaayush9841@gmail.com>
2023-01-02 13:46:39 +0545

    created admin & user rating and review screen

---
+++ lib/AdminScreens/AdminRatingReview.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AdminRatingReview extends StatefulWidget {
+  const AdminRatingReview({Key? key}) : super(key: key);
+
+  @override
+  State<AdminRatingReview> createState() => _AdminRatingReviewState();
+}
+
+class _AdminRatingReviewState extends State<AdminRatingReview> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/UserScreens/UserRatingReview.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UserRatingReview extends StatefulWidget {
+  const UserRatingReview({Key? key}) : super(key: key);
+
+  @override
+  State<UserRatingReview> createState() => _UserRatingReviewState();
+}
+
+class _UserRatingReviewState extends State<UserRatingReview> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
