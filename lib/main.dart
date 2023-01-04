5fb1612cac9e05ce476949ceb43c5aee56d554ff
Sanjeelathapa7 <sanjeelathapamagar1234@gmail.com>
2023-01-04 20:40:28 +0545

    Khalti Payment added with images

--- lib/main.dart
+++ lib/main.dart
@@ -5,20 +5,35 @@ import 'package:chahewoneu/UserScreens/Transportation/ChooseBusSeat.dart';
 import 'package:chahewoneu/UserScreens/Transportation/meansOfTransportation.dart';
 import 'package:chahewoneu/UserScreens/UserLogin.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:khalti_flutter/khalti_flutter.dart';
 
+import 'UserScreens/Payment/KhaltiTest.dart';
 import 'UserScreens/Transportation/ChooseHiaceSeat.dart';
 
 void main() {
-  runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: "/MeansOfTransportation",
-      routes: {
-        "userlogin": (context) => UserLoginPage(),
-        "adminlogin": (context) => AdminLoginScreen(),
-        "admindashboard": (context) => AdminDashboard(),
-        "/ChooseBusSeat": (context) => ChooseBusSeat(),
-        "/ChooseHiaceSeat": (context) => ChooseHiaceSeat(),
-        "/ChooseAirplaneSeat": (context) => Airplane(),
-        "/MeansOfTransportation": (context) => Transportation(),
-      }));
+  runApp(const Myapp());
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+      overlays: [SystemUiOverlay.bottom]);
 }
+
+class Myapp extends StatelessWidget {
+  const Myapp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return KhaltiScope(
+        publicKey: 'test_public_key_5a22ad67e707441b8362fc7bed556a8d',
+        enabledDebugging: true,
+        builder: (context, navKey) {
+          return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              home: KhaltiPayment(),
+              navigatorKey: navKey,
+              localizationsDelegates: const [
+                KhaltiLocalizations.delegate,
+              ]);
+        });
+  }
+}
+
