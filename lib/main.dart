9ed88935e4a3a6984d60c1a698dc78be415b0449
Sanjeelathapa7 <sanjeelathapamagar1234@gmail.com>
2023-01-12 21:03:19 +0545

    initial route changed

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ import 'UserScreens/Transportation/PopUpMenu.dart';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    initialRoute: "/userDashboard",
+    initialRoute: "/userPayment",
     routes: {
       // "/splashScreen": (context)=>SplashScreen();
       "/registration": (context) => RegisterScreen(),
