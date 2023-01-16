4fb0641f96c19d75cbd24a0ce7d912e3806f061f
deeya2002 <diyadangol2002@gmail.com>
2023-01-16 12:22:00 +0545

    created adin events

---
+++ lib/AdminScreens/adminevents.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class AdminEvents extends StatefulWidget {
+  const AdminEvents({Key? key}) : super(key: key);
+
+  @override
+  State<AdminEvents> createState() => _AdminEventsState();
+}
+
+class _AdminEventsState extends State<AdminEvents> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
